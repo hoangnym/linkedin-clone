fd50485c46eea072a99f2018a51e7e1d1a8f03b4
hoangnym <hoangnym94@gmail.com>
2022-05-31 21:17:50 +0200

    create header options

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
 
 function Header() {
     return (
@@ -18,7 +19,8 @@ function Header() {
             </div>
 
             <div className="header__right">
-
+                <HeaderOption title={"Home"}/>
+                <HeaderOption title={"My Network"}/>
             </div>
         </div>
     )

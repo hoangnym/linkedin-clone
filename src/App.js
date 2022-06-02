90ff2be33b9f41184d2b8add9647003f22bea672
hoangnym <hoangnym94@gmail.com>
2022-06-02 13:30:42 +0200

    add login persistence

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 import './App.css';
+import { auth } from './firebase';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
@@ -9,6 +10,22 @@ import Login from './Login';
 
 function App() {
     const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((userAuth) => {
+            if(userAuth) {
+                dispatch(login({
+                    email: userAuth.email,
+                    uid: userAuth.uid,
+                    displayName: userAuth.displayName,
+                    photoUrl: userAuth.photoURL
+                }))
+            } else {
+                dispatch(logout());
+            }
+        })
+    });
 
     return (
     <div className="app">
